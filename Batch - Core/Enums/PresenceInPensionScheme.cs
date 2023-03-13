890c040138d2941a27752c900511c8ee146436d4
Rune Martin Bjerregaard Petersen <G81986-ext@velliv.dk>
2023-03-13 12:16:39 +0100

    Backup

--- Batch - Core/Enums/PresenceInPensionScheme.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DTL.Entities.Enums;
-
-public enum PresenceInPensionScheme
-{
-    Present = 100000000,
-    Replaced = 100000001,
-    Removed = 100000002
-}
--- SandboxFramework/Program.cs
+++ SandboxFramework/Program.cs
@@ -7,22 +7,7 @@ namespace SandboxFramework
     {
         public static void Main(string[] args)
         {
-            // PerformanceTest.CreateAndDeleteParallelExecuteMultiple(2, 200, 1000);
-            // Deletion.Cleanup();
             
-            var organizationService = OrganizationService.GetOrganizationService();
-
-            var notesQuery = new QueryExpression("annotation")
-            {
-                ColumnSet = new ColumnSet("subject", "mimetype", "filename", "documentbody"),
-                Criteria = new FilterExpression
-                    {
-                    FilterOperator = LogicalOperator.And,
-                    Conditions = { new ConditionExpression("objectid", ConditionOperator.Equal, sourceEntityId) }
-                   }
-            };
-            
-            var notesList = organizationService.RetrieveMultiple(notesQuery);
         }
     }
 }
