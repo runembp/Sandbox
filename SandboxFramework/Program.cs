835c47610a166bb821489d93bcb357f63fd07aff
Rune Martin Bjerregaard Petersen <G81986-ext@velliv.dk>
2023-04-05 13:30:12 +0200

    Backup

--- SandboxFramework/Program.cs
+++ SandboxFramework/Program.cs
@@ -1,111 +1,25 @@
 ﻿using System;
 using Microsoft.Xrm.Sdk;
-using Microsoft.Xrm.Sdk.Messages;
 using Microsoft.Xrm.Sdk.Query;
+using SandboxFramework.Entities;
 
 namespace SandboxFramework
 {
     internal static class Program
     {
-        private static readonly IOrganizationService OrganizationService = Tools.OrganizationService.GetOrganizationService();
-        private static ExecuteMultipleRequest _executeMultipleRequest;
-
-        private const string AccountOpportunityLogicalName = "opportunity";
-        private const string AccountOpportunityStartingDateExpectedField = "new_startingdateexpected";
-        private const string AccountOpportunityDaysBeforeStartingDateField = "new_daysbeforestartingdate";
-        private const string AccountOpportunityStatusField = "statuscode";
+        private static IOrganizationService OrganizationService;
 
         public static void Main()
-        {
-            const int openStatus = 1;
+		{
+            OrganizationService = Tools.OrganizationService.GetOrganizationService();
 
-            var accountOpportunitiesQuery = new QueryExpression(AccountOpportunityLogicalName)
-            {
-                ColumnSet = new ColumnSet(AccountOpportunityStartingDateExpectedField, AccountOpportunityStartingDateExpectedField),
-                PageInfo = new PagingInfo
-                {
-                    PageNumber = 1,
-                    // Count = recordsPerPage,
-                    PagingCookie = null
-                },
-                Criteria = new FilterExpression
-                {
-                    Conditions =
-                    {
-                        new ConditionExpression(AccountOpportunityStartingDateExpectedField, ConditionOperator.NotNull),
-                        new ConditionExpression(AccountOpportunityStatusField, ConditionOperator.Equal, openStatus)
-                    }
-                }
-            };
+            var accountEntity = OrganizationService.Retrieve(AccountEntity.EntityLogicalName, new Guid("{D6D9E6A3-75CB-E011-B687-E41F13451D90}"), new ColumnSet());
+            
+            // var accountPath = accountEntity.GetAttributeValue<string>("new_path");
 
-            var accountOpportunities = OrganizationService.RetrieveMultiple(accountOpportunitiesQuery);
+            accountEntity["emailaddress1"] = @"\\smb-app.dev1.vlpadr.net\CRM\FolderTest\";
+            OrganizationService.Update(accountEntity);
 
-            if (accountOpportunities.Entities.Count == 0)
-            {
-                return;
-            }
-
-            _executeMultipleRequest = new ExecuteMultipleRequest
-            {
-                Requests = new OrganizationRequestCollection(),
-                Settings = new ExecuteMultipleSettings
-                {
-                    ContinueOnError = true,
-                    ReturnResponses = true
-                }
-            };
-
-            while (true)
-            {
-                UpdateDaysBeforeExpectedStartingDateField(accountOpportunities.Entities);
-
-                if (!accountOpportunities.MoreRecords)
-                {
-                    break;
-                }
-
-                accountOpportunitiesQuery.PageInfo.PageNumber++;
-                accountOpportunitiesQuery.PageInfo.PagingCookie = accountOpportunities.PagingCookie;
-                accountOpportunities = OrganizationService.RetrieveMultiple(accountOpportunitiesQuery);
-            }
-        }
-
-        private static void UpdateDaysBeforeExpectedStartingDateField(DataCollection<Entity> accountOpportunities)
-        {
-            foreach (var accountOpportunity in accountOpportunities)
-            {
-                var daysBeforeExpectedStartingDate = (DateTime.Now - accountOpportunity.GetAttributeValue<DateTime>(AccountOpportunityStartingDateExpectedField)).Days;
-                var isStartingDateWithin90Days = daysBeforeExpectedStartingDate is <= 90 and >= 0;
-
-                if (!isStartingDateWithin90Days)
-                {
-                    continue;
-                }
-                
-                var accountOpportunityDaysBeforeStartingDateFieldValue = accountOpportunity.GetAttributeValue<int>(AccountOpportunityDaysBeforeStartingDateField);
-
-                if (daysBeforeExpectedStartingDate <= 45 && accountOpportunityDaysBeforeStartingDateFieldValue != 45)
-                {
-                    accountOpportunity[AccountOpportunityDaysBeforeStartingDateField] = 45;
-                }
-                else if(accountOpportunityDaysBeforeStartingDateFieldValue != 90)
-                {
-                    accountOpportunity[AccountOpportunityDaysBeforeStartingDateField] = 90;
-                }
-                else
-                {
-                    continue;
-                }
-                
-                _executeMultipleRequest.Requests.Add(new UpdateRequest
-                {
-                    Target = accountOpportunity
-                });
-
-                var thing = (int) accountOpportunity[AccountOpportunityDaysBeforeStartingDateField];
-            }
-
-            // OrganizationService.Execute(_executeMultipleRequest);
         }
     }
 }
---
+++ SandboxFramework/Tools/CountFieldsOnEntity.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+
+namespace SandboxFramework.Tools;
+
+public class CountFieldsOnEntity
+{
+    public static void CountFields(string entityName)
+    {
+        var organizationService = OrganizationService.GetOrganizationService();
+
+        EntityMetadata entityMetadata = ((RetrieveEntityResponse)organizationService.Execute(new RetrieveEntityRequest
+        {
+            LogicalName = entityName,
+            EntityFilters = EntityFilters.Attributes
+        })).EntityMetadata;
+
+        int existingFieldCount = 0;
+        int optionSetCount = 0;
+        int lookupCount = 0;
+        int customerCount = 0;
+        int ownerCount = 0;
+        int multiSelectPicklistCount = 0;
+
+        foreach (AttributeMetadata attributeMetadata in entityMetadata.Attributes)
+        {
+            switch (attributeMetadata.AttributeType)
+            {
+                case AttributeTypeCode.Picklist:
+                case AttributeTypeCode.State:
+                case AttributeTypeCode.Status:
+                    optionSetCount++;
+                    break;
+                case AttributeTypeCode.Lookup:
+                case AttributeTypeCode.Customer:
+                    lookupCount++;
+                    break;
+                case AttributeTypeCode.Owner:
+                    ownerCount++;
+                    break;
+                // case AttributeTypeCode.MultiSelectPicklistAttributeMetadata:
+                //     multiSelectPicklistCount++;
+                //     break;
+            }
+
+            Console.WriteLine($"{attributeMetadata.LogicalName} ({attributeMetadata.AttributeType}): 1");
+        }
+
+        var remainingFieldCount = 1024 - existingFieldCount - optionSetCount - (2 * lookupCount) - customerCount - ownerCount - multiSelectPicklistCount;
+
+        Console.WriteLine($"Existing fields: {existingFieldCount}");
+        Console.WriteLine($"Option sets: {optionSetCount}");
+        Console.WriteLine($"Lookups and Customers: {lookupCount + customerCount}");
+        Console.WriteLine($"Owners: {ownerCount}");
+        Console.WriteLine($"Multi-select picklists: {multiSelectPicklistCount}");
+        Console.WriteLine($"Remaining fields: {remainingFieldCount}");
+
+        Console.ReadLine();
+    }
+}
